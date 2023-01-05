01994676ea42eb6da4df784bf1dea3166cab494e
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2023-01-05 13:46:57 +0100

    exercisis

---
+++ HighScoreBoard.js
@@ -0,0 +1,30 @@
+export function createScoreBoard() {
+  return {
+    "The Best Ever": 1000000,
+  };
+}
+export function addPlayer(scoreBoard, player, score) {
+  scoreBoard[player] = score;
+  return scoreBoard;
+}
+
+export function removePlayer(scoreBoard, player) {
+  delete scoreBoard[player];
+  return scoreBoard;
+}
+
+export function updateScore(scoreBoard, player, points) {
+  scoreBoard[player] += points;
+  return scoreBoard;
+}
+
+export function applyMondayBonus(scoreBoard) {
+  for (let value in scoreBoard) {
+    scoreBoard[value] += 100;
+  }
+  return scoreBoard;
+}
+
+export function normalizeScore(params) {
+  return params.normalizeFunction(params.score);
+}
